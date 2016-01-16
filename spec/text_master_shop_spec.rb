0f21e55537048e3276f4b12247d210bbe37daa5f
Joe Sak <joe@joesak.com>
2016-01-16 13:55:54 -0500

    0 specs

--- spec/text_master_shop_spec.rb
+++ spec/text_master_shop_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe TextMasterShop do
-  it 'has a version number' do
-    expect(TextMasterShop::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
